63330554eef25b8ba3ff652f9117adf1a4db2849
MindfulLearner <dimajosh1009@gmail.com>
2024-11-25 12:21:03 +0000

    mod/cors modified

--- config/cors.php
+++ config/cors.php
@@ -21,7 +21,7 @@ return [
 
     'allowed_origins' => [
         'http://192.168.1.101:5173',
-        'localhost:5174',
+        'http://localhost:5174',
     ],
 
     'allowed_origins_patterns' => [],
