d82df6ca52872a93b2b7d15e8bcbb944c3e047fa
surefire-hu <surefire.hu2001@gmail.com>
2024-12-03 17:30:50 +0100

    grafica card

--- resources/views/sponsors/createSilver.blade.php
+++ resources/views/sponsors/createSilver.blade.php
@@ -2,14 +2,14 @@
 
 @section('content')
 <div class="flex items-center justify-center min-h-screen bg-gray-100">
-    <div class="max-w-4xl mx-auto bg-white p-6 rounded-lg shadow-md">
+    <div class="max-w-4xl mx-auto bg-gradient-to-r from-gray-300 to-gray-500 p-6 rounded-lg shadow-md">
         <h2 class="text-2xl font-bold mb-4">Seleziona Appartamenti per il Piano Silver</h2>
         {{-- <form action="{{ route('') }}" method="POST"> --}}
             @csrf
             <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
                 <!-- Qui ci fa il foreach apartments as apartment -->
                 {{-- @foreach($apartments as $apartment) --}}
-                    <div class="border p-4 rounded-lg shadow-sm">
+                    <div class="border p-4 rounded-lg shadow-sm bg-white">
                         <h3 class="text-lg font-semibold"> {{--{{ $apartment->name }} --}} </h3>
                         <p class="text-gray-600">{{--{{ $apartment->description }} --}} </p>
                         <p class="text-gray-800 font-bold">Prezzo: 5,99 €</p>
