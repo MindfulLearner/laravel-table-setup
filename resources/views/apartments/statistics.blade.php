738f89608118cbe519f84df30c7cb0d51bd8a032
MindfulLearner <dimajosh1009@gmail.com>
2024-12-04 12:45:08 +0000

    feat/addded pagina statics

--- resources/views/apartments/statistics.blade.php
+++ resources/views/apartments/statistics.blade.php
@@ -13,6 +13,14 @@
             <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mt-3">
                 {{ $views->count() }} {{ __("visualizzazioni") }}
             </div>
+            <div class="mt-3">
+                <p>IP visitatori</p>
+                @foreach ($views as $view)
+                    <p>{{ $view->ip_address }}</p>
+                    <p>{{ $view->created_at }}</p>
+                    <p>{{ $view->id }}</p>
+                @endforeach
+            </div>
         </div>
     </div>
 </div>
