3c81811f63e9fbad46e81a10575fd7080bc19322
MindfulLearner <dimajosh1009@gmail.com>
2024-12-07 13:18:24 +0000

    littlefix/title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>swag</title>
+        <title>MilanBnB</title>
         <!-- Favicon -->
         <link rel="icon" href="{{ asset('logo.png') }}">
 
